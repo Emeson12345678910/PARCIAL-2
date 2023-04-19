9b86a716de62b0f31dc93ca7f923a3b380e34e31
Emeson12345678910 <124007178+Emeson12345678910@users.noreply.github.com>
2023-04-18 20:41:03 -0600

    Add files via upload

---
+++ PARCIAL 2/PARCIAL 2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+ //error  es que trata de sumar un tipo string a un int el cual intenta convertirlo con un 
+ // toint 32 pero este tiene una letra "G" el cual lodeja en string siempre 
+try
+{
+    int dato1 = 16;
+    string dato2= "25g";
+    int Resultado = dato1 + Convert.ToInt32(dato2);
+}
+catch
+{
+    Console.WriteLine("Lo sentimos no se puede realizar su operaciòn ");
+}
